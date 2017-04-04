e97d2f7c531463fbbafae5c120009aac3eb8d70b
FleetFei <qifeish@gmail.com>
2017-04-04 19:40:48 -0400

    qifei

--- WebContent/reveiw_crawler.js
+++ WebContent/reveiw_crawler.js
@@ -17,8 +17,8 @@ function reqWeb(url,id,callback){
 		res.on("end",function(){
 			console.log("suceed linking to ----- >> "+url);
 			 var content =html; //采用cheerio模块解析html
-			 console.log(content);
-			 fs.writeFile(fWriteName+id+".txt",content,function (err) {
+// 			 console.log(content);
+			 fs.writeFile(fWriteName+"/"+id+".txt",content,function (err) {
 				 if (err) throw err ;
 				 console.log("File Saved !"); //文件被保存
 				 callback();
@@ -35,12 +35,17 @@ function reqWeb(url,id,callback){
 
 //main 函数
 var fReadName = path.resolve(__dirname, "../src/gameIDlist.txt"); 
-var fWriteName = path.resolve(__dirname, "../src/Userreview/"); 
+var fWriteName = path.resolve(__dirname, "../src/Data_review/"); 
 fs.readFile(fReadName, function(err, data) {
     	if(err) throw err;
     	//get array of ID
-	    var rawarray = data.toString().split("\n");
+	    var array = data.toString().split("\n");
 	    //crawler
+	    var rawarray =[];
+	    for(var i=12000, j=0; i<array.length; i++,j++){
+			rawarray[j]=array[i];
+		}
+		
 	    async.eachSeries(rawarray,function (id,callback){
 	    	var url = "http://store.steampowered.com//appreviews/"+id+"?start_offset=0&day_range=30&filter=summary&language=english&review_type=positive&purchase_type=all&review_beta_enabled=0";
 	    	reqWeb(url,id,callback);
