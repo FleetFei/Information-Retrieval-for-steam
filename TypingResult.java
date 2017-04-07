8b1470889f57129d7089656cad23ccd359d26fdb
zhaoyingpei99 <yingpei.zhao.2016@hotmail.com>
2017-04-07 10:41:32 -0400

    change search while typing and after press enter

---
+++ TypingResult.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class TypingResult {
+	ArrayList<String> initialResult;
+	
+	TypingResult(ArrayList<relativeName> inputInitialResult) {
+		
+		this.initialResult = new ArrayList<String>();
+		
+		for (relativeName game : inputInitialResult) {
+			this.initialResult.add(game.name);
+		}
+	}
+	
+	void print() {
+		System.out.println("Results only contain Initial words: ");
+		for (String st1 : initialResult) {
+			System.out.println(st1);
+		}
+		System.out.println();
+	}
+	
+
+}
